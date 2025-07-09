60b640100b165e8d87d762165279d643e788cadb
kt <rudxo0525@gmail.com>
2025-07-10 00:33:55 +0900

    Project Settings ended!

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",       // 루트 index.html
+    "./src/**/*.{js,ts,jsx,tsx}" // src 폴더 내 모든 컴포넌트
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
